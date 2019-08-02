a9943a932220b396d9b6a539770a16a78c62524a
julia <yasminjullia@live.com>
2019-08-02 11:22:40 -0300

    Finalização do app

--- app/src/main/java/com/example/gasolinagoals/MainActivity.java
+++ app/src/main/java/com/example/gasolinagoals/MainActivity.java
@@ -27,10 +27,25 @@ public class MainActivity extends AppCompatActivity {
         String precoAlcol = precoAlcool.getText().toString();
         String precoGs = precoGas.getText().toString();
 
-        Boolean campoValido = this.validaCampo(precoAlcol, precoGs){
-
+        Boolean campoValido = this.validaCampo(precoAlcol, precoGs);
+        if(campoValido){
+            this.calculaMelhrPreco(precoAlcol, precoGs);
+        } else{
+            resultado.setText("Preencha os campos primeiro");
         }
     }
+    public void calculaMelhrPreco(String pAlcool, String pGas){
+// Converte valores string para números
+        Double precoAlcool = Double.parseDouble(pAlcool);
+        Double precoGas = Double.parseDouble(pGas);
+        Double resulta = precoAlcool/precoGas;
+        if (precoAlcool>= 0.7){
+            resultado.setText("Melhor utilizar gasolina");
+        } else {
+            resultado.setText("Melhor utilizar Àlcool");
+        }
+    }
+
     public boolean validaCampo (String pAlcool, String pGas){
         Boolean campoValido = true;
 
